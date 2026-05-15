6907bdf5f1575a4dc887731fa72f9816c89b84f0
Al- Mamun Reyad <reyad@skylarkcreative.co.uk>
2026-05-15 11:40:34 +0600

    Add mongoose dependency and implement database connection in layout

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,10 +1,16 @@
 import HotelCard from "./HotelCard";
+import { getAllHotels } from "@/database/queries";
 
-const HotelList = () => {
+const HotelList = async () => {
+  const allHotels = await getAllHotels();
+  console.log(allHotels);
+
   return (
     <div className="col-span-9">
       <div className="space-y-4">
-        <HotelCard />
+        {allHotels.map((hotel) => (
+          <HotelCard key={hotel.id} hotel={hotel} />
+        ))}
       </div>
     </div>
   );
