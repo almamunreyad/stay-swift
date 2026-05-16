6fb613bcc53b91d288b8f5dca11dc3a7c62c37cd
almamunreyad <reyad@websure.digital>
2026-05-16 11:29:56 +0600

    Implement authentication with NextAuth using MongoDB adapter; add login and registration forms with error handling; update Navbar to reflect user session state.

--- auth.js
+++ auth.js
@@ -1,17 +1,60 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import mongoClientPromise from "./database/mongoClientPromise";
+import credentialsProvider from "next-auth/providers/credentials";
+import { userModel } from "./models/review-model";
+import bcrypt from "bcryptjs";
 
 export const {
-    handlers: { GET, POST },
-    auth,
-    signIn,
-    signOut
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
 } = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
+  adapter: MongoDBAdapter(mongoClientPromise, {
+    databaseName: process.env.ENVIRONMENT,
+  }),
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    credentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (credentials === null) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+
+          if (user) {
+            const isMatch = await bcrypt.compare(
+              credentials.password,
+              user.password,
+            );
+
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Invalid credentials");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
